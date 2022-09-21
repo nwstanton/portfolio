2af3fe119df5812b02ebf22e6d756ef02ae0ddc6
Nathan_Stanton <nwstanton@gmail.com>
2022-09-21 18:41:26 -0500

    installed prettier

--- components/ExpCard.tsx
+++ components/ExpCard.tsx
@@ -1,17 +1,17 @@
 function ExpCard() {
-    return (
-        <div className="border rounded border-slate-500 p-4 max-w-{300} hover:shadow-lg">
-            <h2 className='font-bold text-lg text-slate-500'>Talent Guard &rarr;</h2>
-            <h3>Full-Stack Engineer</h3>
-            <ul>
-                <li>C#</li>
-                <li>ASP NET</li>
-                <li>Blazor</li>
-                <li>SQL Server</li>
-                <li>Azure Dev Ops</li>
-            </ul>
-        </div>
-    )
+  return (
+    <div className="max-w-{300} rounded border border-slate-500 p-4 hover:shadow-lg">
+      <h2 className="text-lg font-bold text-slate-500">Talent Guard &rarr;</h2>
+      <h3>Full-Stack Engineer</h3>
+      <ul>
+        <li>C#</li>
+        <li>ASP NET</li>
+        <li>Blazor</li>
+        <li>SQL Server</li>
+        <li>Azure Dev Ops</li>
+      </ul>
+    </div>
+  )
 }
 
 export default ExpCard
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,16 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import ExpCard from '../components/ExpCard'
 
 const Home: NextPage = () => {
   return (
-    <div className='flex items-center flex-col'>
-      <h1 className="text-3xl font-bold underline text-red-500 m-8">
+    <div className="flex flex-col items-center">
+      <h1 className="m-8 text-3xl font-bold text-red-500 underline">
         Hello world!
       </h1>
 
       <ExpCard />
-
-      
     </div>
   )
-
 }
 
 export default Home
