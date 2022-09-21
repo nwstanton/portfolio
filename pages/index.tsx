439c2d4f0d0acc5bc9c58947a0b8e81ed3ffd722
Nathan_Stanton <nwstanton@gmail.com>
2022-09-21 18:22:15 -0500

    Progress with new expCards components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,21 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import ExpCard from '../components/ExpCard'
 
 const Home: NextPage = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">
-      Hello world!
-    </h1>
+    <div className='flex items-center flex-col'>
+      <h1 className="text-3xl font-bold underline text-red-500 m-8">
+        Hello world!
+      </h1>
+
+      <ExpCard />
+
+      
+    </div>
   )
+
 }
 
 export default Home
