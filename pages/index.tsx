a6f5866608f7699820f22dc61ba2266e711c085e
Nathan_Stanton <nwstanton@gmail.com>
2022-09-22 22:46:25 -0500

    No escaped chars fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import type { NextPage } from 'next'
 import Image from 'next/image'
-import profilePic from "../public/images/portfolio picture.jpg"
+import profilePic from '../public/images/portfolio picture.jpg'
 import ExpCard from '../components/ExpCard'
 
 const Home: NextPage = () => {
   return (
     <div className="flex flex-col items-center">
-      <div className='py-5'>
+      <div className="py-5">
         <Image
           src={profilePic}
           alt="NatePic"
@@ -16,7 +16,7 @@ const Home: NextPage = () => {
         />
       </div>
       <h1 className="m-8 text-3xl font-bold text-black">
-        Hi, I'm Nathan Stanton. Lets{' '}
+        Hi, I&apos;m Nathan Stanton. Lets{' '}
         <a
           href="https://www.linkedin.com/in/nathanwaynestanton/"
           target="_blank"
@@ -27,13 +27,13 @@ const Home: NextPage = () => {
         </a>
       </h1>
 
-      <div className='grid grid-cols-2 gap-8'>
+      <div className="grid grid-cols-2 gap-8">
         <ExpCard />
-      
+
         <ExpCard />
-      
+
         <ExpCard />
-     
+
         <ExpCard />
       </div>
     </div>
