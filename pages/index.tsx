07a47cfa200af43dbcb64284a95059bf2c0f4ae0
Nathan_Stanton <nwstanton@gmail.com>
2022-09-22 22:12:44 -0500

    Added grid for expcards and hover affect

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,32 @@ import ExpCard from '../components/ExpCard'
 const Home: NextPage = () => {
   return (
     <div className="flex flex-col items-center">
-      <div>
-        <img src='/images/portfolio picture.jpg' className='rounded-full h-60 w-60'></img>
+      <div className='py-5'>
+        <img
+          src="/images/portfolio picture.jpg"
+          className="h-60 w-60 rounded-full"
+        ></img>
       </div>
-      <h1 className="m-8 text-3xl font-bold text-red-500 underline">
-        Hello world!
+      <h1 className="m-8 text-3xl font-bold text-black">
+        Hi, I'm Nathan Stanton. Lets{' '}
+        <a
+          href="https://www.linkedin.com/in/nathanwaynestanton/"
+          target="_blank"
+          className="text-cyan-500"
+        >
+          Connect.
+        </a>
       </h1>
 
-      <ExpCard />
+      <div className='grid grid-cols-2 gap-8'>
+        <ExpCard />
+      
+        <ExpCard />
+      
+        <ExpCard />
+     
+        <ExpCard />
+      </div>
     </div>
   )
 }
