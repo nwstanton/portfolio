0ecf1c0290628ecfed45cf723acd55810361be75
Nathan_Stanton <nwstanton@gmail.com>
2022-09-30 17:51:18 -0500

    Added expand symbol

--- components/ExpCard.tsx
+++ components/ExpCard.tsx
@@ -29,13 +29,18 @@ summary : string
   const content = front ? (
     <>
     <div className="max-w-[210px] rounded border border-slate-500 from-green-300 to-blue-400 p-4 hover:border-white hover:bg-gradient-to-r hover:shadow-lg">
-    <h2 className="text-lg font-bold text-black">{name} &rarr;</h2>
-    <h3>{position}</h3>
-    <ul className="list-disc list-outside mx-6 mb-4">
-      {experience.map((value: string, index: number) => (
-            <li key={index}>{value}</li>
-          ))}
-    </ul>
+      <div className="relative left-[120px] top-0">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15" />
+        </svg>
+      </div>
+      <h2 className="text-lg font-bold text-black">{name}</h2>
+      <h3>{position}</h3>
+      <ul className="list-disc list-outside mx-6 mb-4">
+        {experience.map((value: string, index: number) => (
+              <li key={index}>{value}</li>
+            ))}
+      </ul>
     </div>
     </>
   ) :
